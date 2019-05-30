bd64354bf75276730ec4c5096c5c2ea39de0e15a
Amanda Garcia <algarcian@outlook.com>
2019-05-30 14:42:49 -0600

    Add images and set up variables

---
+++ app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  var user_choice_p = $(".user-choice p"); 
+  var comp_choice_p = $(".comp-choice p"); 
+  var results_p = $(".results p")
+  var choices = $(".choice"); 
+  var userScore_span = $("#user-score"); 
+  var compScore_span = $("#comp-score");
+  var user_label_div = $("#user"); 
+  var comp_label_div = $("#comp"); 
+  
+  // CHOICES 
+  var hobbit = $("#hobbit"); 
+  var nazgul = $("#nazgul"); 
+  var sauron = $("#sauron"); 
+
+  var options = ["hobbit", "nazgul", "sauron"]
+  var userChoice; 
+  var compChoice; 
+  var userScore = 0; 
+  var compScore = 0; 
+
+  // will grab all choice classes on click 
+  choices.on("click", function() {
+
+  }); 
+  
+  
+
+
+
+
+
+
+
+}); // end document ready function 
